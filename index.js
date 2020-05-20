b2fd85b566a99a1bc779048398e12813e6420827
Gerald Maboshe <gerald@thezig.io>
2020-05-20 22:44:14 +0200

    quick-start

--- index.js
+++ index.js
@@ -1,68 +1,50 @@
-'use strict';
+/**
+ * Copyright 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Messenger Platform Quick Start Tutorial
+ *
+ * This is the completed code for the Messenger Platform quick start tutorial
+ *
+ * https://developers.facebook.com/docs/messenger-platform/getting-started/quick-start/
+ *
+ * To run this code, you must do the following:
+ *
+ * 1. Deploy this code to a server running Node.js
+ * 2. Run `npm install`
+ * 3. Update the VERIFY_TOKEN
+ * 4. Add your PAGE_ACCESS_TOKEN to your environment vars
+ *
+ */
 
+'use strict';
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
-  dotenv = require('dotenv').config();
+  app = express().use(body_parser.json()); // creates express http server
 
-var app = express();
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-app.set('port', process.env.PORT || 5000);
-app.use(body_parser.json());
-app.use(express.static('public'));
-
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const SERVER_URL = process.env.SERVER_URL;
-const APP_SECRET = process.env.APP_SECRET;
-
-app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-module.exports = app;
-
-// Serve the options path and set required headers
-app.get('/options', (req, res, next) => {
-  let referer = req.get('Referer');
-  if (referer) {
-    if (referer.indexOf('www.messenger.com') >= 0) {
-      res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
-    } else if (referer.indexOf('www.facebook.com') >= 0) {
-      res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
-    }
-    res.sendFile('public/options.html', { root: __dirname });
-  }
-});
-
-// Handle postback from webview
-app.get('/optionspostback', (req, res) => {
-  let body = req.query;
-  let response = {
-    text: `Great, I will book you a ${body.bed} bed, with ${body.pillows} pillows and a ${body.view} view.`
-  };
-
-  res
-    .status(200)
-    .send('Please close this window to return to the conversation thread.');
-  callSendAPI(body.psid, response);
-});
-
-// Accepts POST requests at the /webhook endpoint
+// Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-    body.entry.forEach(entry => {
+    body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
 
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
-      console.log(`Sender PSID: ${sender_psid}`);
+      console.log('Sender ID: ' + sender_psid);
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
@@ -72,7 +54,6 @@ app.post('/webhook', (req, res) => {
         handlePostback(sender_psid, webhook_event.postback);
       }
     });
-
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
   } else {
@@ -83,7 +64,8 @@ app.post('/webhook', (req, res) => {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = process.env.TOKEN;
+  /** UPDATE YOUR VERIFY TOKEN **/
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
@@ -104,30 +86,45 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-// Handles messages events
 function handleMessage(sender_psid, received_message) {
   let response;
 
   // Checks if the message contains text
   if (received_message.text) {
-    switch (
-      received_message.text
-        .replace(/[^\w\s]/gi, '')
-        .trim()
-        .toLowerCase()
-    ) {
-      case 'room preferences':
-        response = setRoomPreferences(sender_psid);
-        break;
-      default:
-        response = {
-          text: `You sent the message: "${received_message.text}".`
-        };
-        break;
-    }
-  } else {
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
     response = {
-      text: `Sorry, I don't understand what you mean.`
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`
+    };
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title: 'Is this the right picture?',
+              subtitle: 'Tap a button to answer.',
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: 'postback',
+                  title: 'Yes!',
+                  payload: 'yes'
+                },
+                {
+                  type: 'postback',
+                  title: 'No!',
+                  payload: 'no'
+                }
+              ]
+            }
+          ]
+        }
+      }
     };
   }
 
@@ -135,32 +132,22 @@ function handleMessage(sender_psid, received_message) {
   callSendAPI(sender_psid, response);
 }
 
-// Define the template and webview
-function setRoomPreferences(sender_psid) {
-  let response = {
-    attachment: {
-      type: 'template',
-      payload: {
-        template_type: 'button',
-        text:
-          "OK, let's set your room preferences so I won't need to ask for them in the future.",
-        buttons: [
-          {
-            type: 'web_url',
-            url: SERVER_URL + '/options',
-            title: 'Set preferences',
-            webview_height_ratio: 'compact',
-            messenger_extensions: true
-          }
-        ]
-      }
-    }
-  };
+function handlePostback(sender_psid, received_postback) {
+  console.log('ok');
+  let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
 
-  return response;
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { text: 'Thanks!' };
+  } else if (payload === 'no') {
+    response = { text: 'Oops, try sending another image.' };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
@@ -169,7 +156,7 @@ function callSendAPI(sender_psid, response) {
     },
     message: response
   };
-  console.log(request_body);
+
   // Send the HTTP request to the Messenger Platform
   request(
     {
